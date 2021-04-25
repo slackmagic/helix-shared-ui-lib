d86361257a32734537fb7d3e1bf25b8ea44d4725
SlackMagiC <laurent.pietrzyk@gmail.com>
2021-04-25 23:17:58 +0200

    ✏️ Fix export typo

--- src/index.tsx
+++ src/index.tsx
@@ -64,6 +64,6 @@ export * from "./services/HelixUserAPI";
 export { default as Login } from "./components/login/Login";
 export * from "./components/login/Login";
 
-export { default as LMaintTheme } from "./components/theme/MainTheme";
+export { default as MaintTheme } from "./components/theme/MainTheme";
 export * from "./components/theme/MainTheme";
 
